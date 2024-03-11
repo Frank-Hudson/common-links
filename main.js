129a6935abab10daf8016461cc674a5363fc1ba5
Frank Hudson <117737645+Frank-Hudson@users.noreply.github.com>
2024-03-11 15:49:20 +0000

    Refactor manufactured HTML elements into strings

--- main.js
+++ main.js
@@ -10,30 +10,37 @@ const SEPARATOR = "#";
 
 const CURRENT_PAGE = window.location.href.split(SEPARATOR)[1];
 
+function anchorString(href, innerHTML) {
+    return `<a href="${href}">${innerHTML}</a>`;
+}
+
+function listitemString(id, innerHTML) {
+    return `<li id="${id}">${innerHTML}</li>`;
+}
+
+function ulistString(id, innerHTML) {
+    return `<ul id="${id}">${innerHTML}</ul>`;
+}
+
 const header = document.getElementById("header");
 const header_heading1 = document.getElementById("header-heading-1");
 
 const page = document.getElementById("page");
 
-const commonLinks_ulist = new HTMLUListElement();
-commonLinks_ulist.id = "common-links";
+let commonLinks_ulist = [];
 
 for (const [subject, links] in COMMON_LINKS_COLLECTION) {
     // If the current page is home
     if (!CURRENT_PAGE || CURRENT_PAGE == "" || CURRENT_PAGE == "#") {
         // Create an <a> element that has the targeted `subject` as its content 
         // and href
-        const subject_anchor = new HTMLAnchorElement();
-        subject_anchor.href = SEPARATOR + subject;
-        subject_anchor.append(subject);
+        const subject_anchor = anchorString(SEPARATOR + subject, subject);
         // Create a <li> that has the targeted `subject` as its id, and append 
         // the previous <a> element to it
-        const subject_listitem = new HTMLLIElement();
-        subject_listitem.id = subject.toLocaleLowerCase();
-        subject_listitem.appendChild(subject_anchor);
+        const subject_listitem = listitemString(subject.toLocaleLowerCase(), subject_anchor);
         // Append the previously created <li> element to the `commonLinks_ulist`
         //  <ul> element
-        commonLinks_ulist.appendChild(subject_listitem);
+        commonLinks_ulist.append(subject_listitem);
     } else {
         // If the subject is not the current page; continue the loop until it 
         // is
@@ -47,21 +54,17 @@ for (const [subject, links] in COMMON_LINKS_COLLECTION) {
         for (const link in links) {
             // Create an <a> element with the current `link` as its href and 
             // content
-            const link_anchor = new HTMLAnchorElement();
-            link_anchor.href = link;
-            link_anchor.append(link);
+            const link_anchor = anchorString(link, link);
             // Create a <li> element with the current `link` as its id and 
             // append the previous <a> element
-            const link_listitem = new HTMLLIElement();
-            link_listitem.id = link;
-            link_listitem.appendChild(link_anchor);
+            const link_listitem = listitemString(link, link_anchor);
             // Append the previously created <li> element to the 
             // `commonLinks_ulist` <ul> element
-            commonLinks_ulist.appendChild(link_listitem);
+            commonLinks_ulist.append(link_listitem);
         }
     }
 }
 
 // Append the `commonLinks_ulist` <ul> element to the `page` <div> element.
-page.appendChild(commonLinks_ulist);
+page.append(ulistString("common-links", commonLinks_ulist));
 const commonLinks = document.getElementById("common-links");
